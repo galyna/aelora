67744b7dbafc629e2613d6c7992b0bb895c133a5
Galyna Krupska <galynavistovska@gmail.com>
2025-05-31 21:39:45 +0300

    initial site components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,23 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import printforgeLogo from "@/public/printforge-logo-1.png";
+import artboard1 from "@/public/Artboard1.png";
 export default function Header() {
   return (
     <header className="bg-white shadow-md">
       <nav className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="relative">
-          <img
-              src="/printforge-logo 1.png"
+            <Image
+              src={printforgeLogo}
               alt="Logo"
               className="w-[200px] h-auto hidden md:block"
+            ></Image>
+            <Image
+              src={artboard1}
+              alt="Logo"
+              className="w-[40px] h-auto block md:hidden"
             />
-            <img src="/Artboard1.png" alt="Logo" className="w-[40px] h-auto block md:hidden" />
-        
           </div>
           <ul className="flex space-x-10">
             <li>
