b059e8a245bac0f35a9a6988254575577ead64e2
galyna <galynakrupska9@gmail.com>
2025-06-11 22:16:52 +0300

    fix: Hero and Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full z-50">
-      <div className="max-w-6xl mx-auto px-6 py-8 flex justify-between items-center">
+      <div className="max-w-full px-8 py-8 flex justify-between place-items-baseline  xl:mx-20">
         {/* Логотип или название */}
-        <Link href="/" className="text-3xl md:text-4xl font-serif text-graphite tracking-wide font-semibold">
+        <Link href="/" className="text-3xl  md:text-5xl font-serif text-graphite tracking-wide 2xl:hidden ">
           Aelora
         </Link>
 
         {/* Навигация — можно оставить один элемент или скрыть */}
-        <nav className="hidden md:flex items-center space-x-10 text-lg text-gray-700 uppercase tracking-wide font-semibold">
+        <nav className="hidden md:flex  2xl:justify-start space-x-10 text-xl lg:text-3xl text-graphite font-semibold">
           <Link href="#skin" className="hover:text-graphite transition">Skin Care</Link>
           <Link href="#hands" className="hover:text-graphite transition">Hand & Body</Link>
           <Link href="#subscribe" className="hover:text-graphite transition">Subscribe</Link>
